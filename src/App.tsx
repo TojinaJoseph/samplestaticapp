4ba180ad79bd7b1653d37c3741debb56b3ff8b88
Tojina Joseph <tojina.joseph@reflectionsinfos.com>
2025-03-04 19:54:25 +0530

    api change

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [message, setMessage] = useState("");
 
     useEffect(() => {
-        fetch("https://myazurefunction1235.azurewebsites.net/api/httpTrigger1?name=Tojina")
+        fetch("https://myazurefunction1235.azurewebsites.net/api/httpTrigger1?")
             .then(response => response.json())
             .then(data => setMessage(data.message))
             .catch(error => console.error("Error fetching API:", error));
