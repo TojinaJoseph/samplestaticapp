d6fef7612ea9638b9e49ca6542705a6688f46573
Tojina Joseph <tojina.joseph@reflectionsinfos.com>
2025-03-04 19:12:11 +0530

    added serverless function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
+  const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch("https://myazurefunction1235.azurewebsites.net/api/httpTrigger1?name=Tojina")
+            .then(response => response.json())
+            .then(data => setMessage(data.message))
+            .catch(error => console.error("Error fetching API:", error));
+    }, []);
   return (
     <>   
-      <h1>Welcome to my updated react webapp</h1>     
+      <h1>{message} ,Welcome to my updated react webapp</h1>     
     </>
   )
 }
