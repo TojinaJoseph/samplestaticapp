9e48fba0588b0b60b9d2286f9f75ff1bd2a9e352
Tojina Joseph <tojina.joseph@reflectionsinfos.com>
2025-03-26 16:07:02 +0530

    updated tasks

--- src/components/tasks/Task.tsx
+++ src/components/tasks/Task.tsx
@@ -6,10 +6,7 @@ function Task() {
     console.log(message)
     useEffect(() => {
       fetch("https://javscriptfunction.azurewebsites.net/api/getDbdata?")
-      .then(response => {
-        console.log("Raw Response:", response); // Logs the full response object
-        return response.json();
-      })
+      .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error("Error fetching API:", error));
     }, []);
